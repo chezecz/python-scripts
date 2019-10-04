4878a9393672b3e5af5633efeb9b54172b488402
chezecz <cheze92@gmail.com>
2019-10-04 10:51:34 +1000

    update the 'construct_sql' function

--- misc_files/update_unit_codes.py
+++ misc_files/update_unit_codes.py
@@ -4,6 +4,13 @@ wb = openpyxl.load_workbook('../Stock.xlsx')
 
 ws = wb['ItemStockroomLocationsExport31']
 
+def construct_query(warehouse, location, item, unit_code_2):
+	query = f"""update itemloc
+				set inv_acct_unit2 = {unit_code_2}
+				where item = {item}
+				and loc = {location}
+				and whse = {warehouse}"""
+	print(query)
+
 for i in range(2, ws.max_row):
-    #construct_sql_query(ws.cell(column = 1, row = i).value, round(ws.cell(column = 7, row = i).value, 4))
-    construct_exec_query(ws.cell(column = 1, row = i).value)
+    construct_query(ws.cell(column = 1, row = i).value, ws.cell(column = 2, row = i).value, ws.cell(column = 3, row = i).value, ws.cell(column = 6, row = i).value)
