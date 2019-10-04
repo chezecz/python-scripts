39a17db1ef476576537a81af337c5793cff05d3d
Syteline Admin <syteline_admin@lemacent.com.au>
2019-10-04 15:50:34 +1000

    work on regex

--- misc_files/update_unit_codes.py
+++ misc_files/update_unit_codes.py
@@ -1,8 +1,11 @@
 import openpyxl
+import re
 
 wb = openpyxl.load_workbook('../../Stock.xlsx')
 
 ws = wb['ItemStockroomLocationsExport31']
+
+pattern = ""
 
 def construct_query(warehouse, location, item, unit_code_2, lbr_code_2, fixed_unit_code_2, var_unit_code_2, out_unit_code_2):
     query = f"""update itemloc
@@ -25,6 +28,9 @@ for i in range(2, ws.max_row):
     fixed_unit_code_2 = ws.cell(column = 18, row = i).value
     var_unit_code_2 = ws.cell(column = 24, row = i).value
     out_unit_code_2 = ws.cell(column = 30, row = i).value
+
+    # if (re.search(r"\B'\w+'\B", item)):
+        # item = re.sub(r"\B'\w+'\B", "'"+str(item)+"'", item)
 
     if (unit_code_2 != ''):
         construct_query(warehouse, location, item, unit_code_2, lbr_code_2, fixed_unit_code_2, var_unit_code_2, out_unit_code_2)
